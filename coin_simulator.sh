4f8781d784f8d6d2852bc6854e16066a3ff51343
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-06 12:43:02 +0530

    how many times win head or tail

--- coin_simulator.sh
+++ coin_simulator.sh
@@ -1,10 +1,18 @@
 #! /bin/bash -x
 
-flipCoin=$((RANDOM%2))
+read -p "how many times wnts flip coin :" Flip
+Tail=0;
+Head=0;
+for ((i=1; i<$Flip; i++)) 
+do
+   flipCoin=$((RANDOM%2))
 
-if [ $flipCoin -eq 1 ]
-then
-    echo "TAIL"
-else
-    echo "HEAD"
-fi
+	if [ $flipCoin -eq 1 ]
+	then
+		((Tail++))
+	else
+	    	((Head++))
+	fi
+done
+ echo "Head win $Tail Times"
+ echo "Tail win $Head Times"
